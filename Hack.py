9615cce0968f3827afa2a957b900d29f0f9fb24d
RAPA-MOKOH <81744824+RAPA-MOKOH@users.noreply.github.com>
2021-04-01 16:43:19 +0800

    Add files via upload

---
+++ Hack.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import time
+
+os.system('clear')
+print
+print
+print
+print
+os.system('figlet SELAMAT ANDA')
+os.system('figlet KENA PRANK')
+print
+time.sleep(2)
+print('NOTE : NANTIKAN PRANK SELANJUTNYA HANYA DI')
+time.sleep(1)
+print('       RUMAH KOK KAYU')
